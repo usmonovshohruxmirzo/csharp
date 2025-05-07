e8868bc6207af20182743ae150706b3aaef861c2
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-05-07 15:17:58 +0500

    feat: Arrays

---
+++ Lessons/Arrays/Arrays/Program.cs
@@ -0,0 +1,121 @@
+﻿namespace Arrays
+{
+   public static class Program
+    {
+        public static void Main()
+        {
+            //Arrays
+            int[] numbers = new int[10]; // All values are 0
+            string[] messages = new string[10]; // All values are null.
+
+            int[] array1 = new int[5];
+
+            int[] array2 = [1, 2, 3, 4, 5, 6];
+
+            int[,] multiDimensionalArray1 = new int[2, 3]; // 2 rows, 3 columns
+
+            int[,] multiDimensionalArray2 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            int[][] jaggedArray = new int[6][];
+            jaggedArray[0] = [1, 2, 3, 4];
+
+            // Collection expressions:
+            //int[] array = [1, 2, 3, 4, 5, 6];
+            // Alternative syntax:
+            //int[] array2 = { 1, 2, 3, 4, 5, 6 };
+
+            //Single-dimensional arrays
+            int[] array = new int[5];
+            string[] weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+            Console.WriteLine(weekDays[0]);
+
+            //Pass single-dimensional arrays as arguments
+            DisplayArray(weekDays);
+            Console.WriteLine();
+
+            ChangeArray(weekDays);
+            DisplayArray(weekDays);
+            Console.WriteLine();
+
+            ChangeArrayElements(weekDays);
+            DisplayArray(weekDays);
+
+            // Multidimensional arrays
+            int[,] array2DDeclaration = new int[4, 2]; 
+            int[,,] array3DDeclaration = new int[4, 2, 3];
+
+            int[,] array2DInitialization = { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } };
+            int[,,] array3D = new int[,,]
+            {
+                {
+                    { 1, 2, 3 },     // array3D[0,0,x]
+                    { 4, 5, 6 }      // array3D[0,1,x]
+                },
+                {
+                    { 7, 8, 9 },     // array3D[1,0,x]
+                    { 10, 11, 12 }   // array3D[1,1,x]
+                }
+            };
+
+
+            System.Console.WriteLine(array2DInitialization[1, 0]);
+            System.Console.WriteLine(array3D[1, 1, 1]);
+            Console.WriteLine(array3D.Rank);
+
+            // Getting the total count of elements or the length of a given dimension.
+            var allLength = array3D.Length;
+            var total = 1;
+            for (int i = 0; i < array3D.Rank; i++)
+            {
+                total *= array3D.GetLength(i);
+            }
+            System.Console.WriteLine($"{allLength} equals {total}");
+
+            int[,] numbers2D = { { 9, 99 }, { 3, 33 }, { 5, 55 } };
+            foreach(int i in numbers2D)
+            {
+                System.Console.Write($"{i} ");
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < array3D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array3D.GetLength(1); j++)
+                {
+                    for (int k = 0; k < array3D.GetLength(2); k++)
+                    {
+                        Console.Write($"{array3D[i, j, k]} ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+            // Pass multidimensional arrays as arguments
+            Print2DArray(new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 }, { 7, 8 } });
+        }
+
+        public static void DisplayArray(string[] arr) => Console.WriteLine(string.Join(" ", arr));
+
+        public static void ChangeArray(string[] arr) => Array.Reverse(arr);
+
+        public static void ChangeArrayElements(string[] arr)
+        {
+            arr[0] = "Changed";
+            arr[1] = "Changed";
+            arr[2] = "Changed";
+        }
+
+        public static void Print2DArray(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.WriteLine($"Element({i}, {j}) = {arr[i, j]}");
+                }
+            }
+        }
+
+    }
+}
