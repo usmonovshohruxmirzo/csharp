d6b892044dfa31b4342939d5c78c7f2e02526bf4
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-08-30 17:43:47 +0500

    feat(Lessons): add Operators

---
+++ Lessons/Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿// null-coalescing operators
+string str = null;
+Console.WriteLine(str ?? "Unkown String");
+
+// str ??= "new string";
+// Console.WriteLine(str);
+
+if (str is null)
+{
+  str = "Another way";
+} 
+Console.WriteLine(str);
+
+
+string notNullStr = "hello";
+
+notNullStr ??= "String is Null";
+Console.WriteLine(notNullStr);
