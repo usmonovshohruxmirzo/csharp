4c5ad94a0aded167a90829fb9e91ead73dda15e7
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-01 20:52:16 +0500

    refactor: add String interpolation, StringBuilder
    - update string formatting info

--- Lessons/Strings/Program.cs
+++ Lessons/Strings/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Strings
+﻿using System.Globalization;
+using System.Text;
+
+namespace Program
 {
     class Program
     {
@@ -46,7 +49,11 @@
             Console.WriteLine(outStr.IndexOf("fox"));
 
 
-            //  INFO: String Formatting =================================================
+            // INFO: String Formatting =================================================
+            // General Format is {index[,alignment]:[format]}
+            // Common Types are N (Number), G (General), F (Fixed-point)
+            // E (Exponential), D (Decimal), G (General), X (Xexadecimal), C (Currency in local format)
+
             int[] quaters = { 1, 2, 3, 4 };
             int[] sales = { 10000, 20000, 3000, 4000 };
             double[] intlMixPct = { .385, .413, .422, .455 };
@@ -58,7 +65,66 @@
 
             Console.WriteLine("{0,12} {1,12} {2,12} {3,12}", quaters[0], quaters[1], quaters[2], quaters[3]);
             Console.WriteLine("{0,12:C2} {1,12:C0} {2,12:C0} {3,12:C0}", sales[0], sales[1], sales[2], sales[3]);
+
+            CultureInfo euroCulture = new CultureInfo("de-DE");
+
+            Console.WriteLine("{0,12:C2} {1,12:C0} {2,12:C0} {3,12:C0}",
+                sales[0].ToString("C2", euroCulture),
+                sales[1].ToString("C0", euroCulture),
+                sales[2].ToString("C0", euroCulture),
+                sales[3].ToString("C0", euroCulture));
+
             Console.WriteLine("{0,12:P0} {1,12:P0} {2,12:P1} {3,12:P2}", intlMixPct[0], intlMixPct[1], intlMixPct[2], intlMixPct[3]);
+
+            // INFO: String Interpolation
+
+            string make = "Mercedes-Benz";
+            string model = "G Class";
+            int year = 2020;
+            float miles = 8_450.27f;
+            decimal price = 60_275.0m;
+            Console.WriteLine("This car is a {0} {1} {2}, with {3} miles and costs {4:C2}", year, make, model, miles, price);
+
+            Console.WriteLine($"This car is a {year} {make} {model}, with {miles} miles and costs {price:C2}");
+
+            Console.WriteLine($"This car is a {year} {make} {model}, with {{{miles * 1.6:F2}}} miles and costs {price:C2}");
+
+            // INFO: StringBuilder
+
+            StringBuilder sb = new StringBuilder("Initial String. ", 200);
+            int jumpCount = 10;
+            string[] animals = { "goats", "cats", "pigs" };
+
+            // print some basic stats about the StringBuilder
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+
+            // Add some strings to the builder using Append
+            sb.Append("The quick brown fox ");
+            sb.Append("jumps over the lazy dog.");
+
+            // AppendLine can append a line ending
+            sb.AppendLine();
+
+            // AppendFormat can be used to append formatted strings
+            sb.AppendFormat("He did this {0} times.", jumpCount);
+            sb.AppendLine();
+
+            // AppendJoin can iterate over a set of values
+            sb.Append("He also jumped over ");
+            sb.AppendJoin(",", animals);
+
+            // Modify the content using Replace
+            sb.Replace("fox", "cat");
+
+            // Insert content at any index
+            sb.Insert(0, "This is the ");
+
+            Console.WriteLine($"Capacity: {sb.Capacity}; Length: {sb.Length}");
+
+            // Convert to a single string
+            Console.WriteLine(sb.ToString());
+
+            // INFO: String Parsing
         }
     }
 }
