9b9eff2662cec9749a6503bee3c380a764274294
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-02 10:49:26 +0500

    refactor(OOP): add computed prop, auto-generated prop, [SetsRequiredMembers]

--- Lessons/ObjectOrientedProgramming/Concepts/Classes.cs
+++ Lessons/ObjectOrientedProgramming/Concepts/Classes.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace ObjectOrientedProgramming.Concepts
 {
     public class Classes
@@ -6,6 +8,22 @@ namespace ObjectOrientedProgramming.Concepts
         {
             Book b1 = new Book("Hello", "World", 800);
             Console.WriteLine(b1.GetDescription());
+            b1.SetName("JS");
+            Console.WriteLine(b1.GetDescription());
+
+            Console.WriteLine(b1.Name);
+            Console.WriteLine(b1.Author);
+
+            b1.ISBN = "1234654894312";
+            b1.Price = 24.58m;
+            Console.WriteLine(b1.ISBN);
+            Console.WriteLine(b1.Price);
+
+            b1.Name = "Crime and Punishment";
+            b1.PageCount = 454;
+            Console.WriteLine(b1.Description);
+            Console.WriteLine(b1.Name);
+            Console.WriteLine(b1.PageCount);
         }
     }
 }
@@ -23,7 +41,7 @@ class Book
         _pageCount = pageCount;
     }
 
-    // Backing Field
+    // INFO: Backing Field
     public string Name
     {
         get
@@ -36,6 +54,7 @@ class Book
         }
     }
 
+    // INFO: expression-bodied properties
     public string Author
     {
         get => _author;
@@ -48,8 +67,49 @@ class Book
         set => _pageCount = value;
     }
 
+    // INFO: Computed property
+    public string Description
+    {
+        get => $"{Name} by {Author}, {PageCount} pages";
+    }
+
+    // INFO: Auto-generated properties
+    public string ISBN
+    {
+        get; set;
+    }
+
+    public decimal Price
+    {
+        get; set;
+    }
+
+
     public string GetDescription()
     {
-        return $"{_name} by {_author}";
+        return $"{_name} by {_author}, {_pageCount} pages";
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string s)
+    {
+        _name = s;
+    }
+}
+
+class Person
+{
+    public required string Name { get; set; }
+    public string Job { get; private set; }
+
+    [SetsRequiredMembers]
+    public Person(string name, string job)
+    {
+        Name = name;
+        Job = job;
     }
 }
