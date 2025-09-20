73a71be64f5f3cf821c55979f3dc22ac7fb57dde
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-09-20 16:08:08 +0500

    feat(Lessons): add Interfaces concept

--- Lessons/Interfaces/Program.cs
+++ Lessons/Interfaces/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document d = new Document("Test Document");
+            d.Save();
+            d.Load();
+        }
+    }
+}
+
+interface ISortable
+{
+    void Save();
+    void Load();
+    bool NeedsSave { get; set; }
+}
+
+class Document : ISortable
+{
+    private string name;
+    public bool needsSave;
+
+    public Document(string s)
+    {
+        name = s;
+        Console.WriteLine("Created a document with name '{0}'", s);
+    }
+
+    public void Save()
+    {
+      Console.WriteLine("Saving the document");
+    }
+
+    public void Load()
+    {
+      Console.WriteLine("Loading the document");
+    }
+
+    public bool NeedsSave
+    {
+        get { return needsSave; }
+        set { needsSave = value; }
+    }
+}
