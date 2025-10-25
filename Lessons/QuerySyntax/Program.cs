a0f6689907bd5a47888e199261353b943f425890
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-25 12:01:33 +0500

    feat(lessons): add QuerySyntax concept

---
+++ Lessons/QuerySyntax/Program.cs
@@ -0,0 +1,10 @@
+﻿List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+var evenNumbers = from n in numbers
+                where n % 2 == 0
+                select n;
+
+foreach (var num in evenNumbers)
+{
+  Console.WriteLine(num);
+}
