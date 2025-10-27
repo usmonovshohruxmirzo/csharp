6086647869112dfeefa05d1467d5852637d3eee0
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-27 13:17:04 +0500

    chore: add Collections concept

---
+++ Lessons/Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Collections");
+        }
+    }
+}
