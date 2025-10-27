19dd2ccb305f2f5c0407967692f5c5f88a256894
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-27 18:25:05 +0500

    feat(collections): add generic collection List, implemented all core List methods
    - added Display method for displaying list
    - added CountElements for conting elements in list

--- Lessons/Collections/Program.cs
+++ Lessons/Collections/Program.cs
@@ -6,7 +6,8 @@ namespace MyNamespace
     {
         static void Main(string[] args)
         {
-            NonGenericCollections.Run();
+            // NonGenericCollections.Run();
+            GenericCollections.Run();
         }
     }
 }
@@ -47,5 +48,118 @@ class NonGenericCollections
         table["age"] = 18;
 
         Console.WriteLine(table["name"]);
+    }
+}
+
+class GenericCollections
+{
+    public static void Run()
+    {
+        Console.WriteLine("\n ============== List ============== \n");
+
+        List<string> cars = new List<string> { "Tesla", "Ferrari", "Audi" };
+
+        // INFO: ADD ELEMENTS
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("ADD ELEMENTS");
+        cars.Add("Honda");
+        cars.AddRange(new List<string> { "Ford", "Toyota" });
+        Display(cars);
+
+        // INFO: INSERT ELEMENTS
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("INSERT ELEMENTS");
+        cars.Insert(2, "Nissan");
+        cars.InsertRange(1, new List<string> { "Volvo", "Mazda" });
+        Console.WriteLine();
+        Display(cars);
+
+        // INFO: REMOVE ELEMENTS
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine("REMOVE ELEMENTS");
+        cars.Remove("Toyota");
+        cars.RemoveAt(0);
+        Display(cars);
+        cars.RemoveRange(2, 4);
+        Console.WriteLine();
+        Display(cars);
+
+        // INFO: SEARCHING
+        Console.WriteLine();
+        Console.WriteLine($"\nContains Toyota? {cars.Contains("Volvo")}");
+        Console.WriteLine($"Index of Nissan: {cars.IndexOf("Mazda")}");
+        Console.WriteLine($"Last Index of Toyota: {cars.LastIndexOf("Ford")}");
+
+        // INFO: SORTING & REVERSING
+        List<int> nums = new List<int> { 2, 3, 4, 8, 6, 9, 8, 1 };
+        nums.Sort();
+        Console.Write("Sorted: ");
+        Display<int>(nums);
+        Console.WriteLine();
+        Console.Write("Reversed: ");
+        nums.Reverse();
+        Display(nums);
+
+        // INFO: CAPACITY & COUNT
+        Console.WriteLine($"\nCount: {cars.Count}");
+        Console.WriteLine($"Capacity: {cars.Capacity}");
+
+        // INFO: CLEAR LIST
+        cars.Clear();
+        Console.WriteLine($"\nAfter Clear: Count = {cars.Count}");
+
+        // INFO: CAPACITY
+
+        List<string> cars2 = new List<string>(100);
+        Console.WriteLine($"Initial Capacity: {cars.Capacity}");
+        Console.WriteLine($"Initial Count: {cars.Count}");
+        cars2.Add("Honda");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        cars2.Add("Toyota");
+        Console.WriteLine($"After Adding 2: Count = {cars2.Count}, Capacity = {cars2.Capacity}");
+        
+        Console.WriteLine(new string('A', 10));
+    }
+
+     public static void Display<T>(List<T> list)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write(string.Join(", ", list));
+        Console.ResetColor();
+    }
+
+     public static void Display(List<string> list)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.Write(string.Join(", ", list));
+        Console.ResetColor();
+    }
+
+    public static void CountElements(List<string> cars)
+    {
+        Console.WriteLine(cars.Count());
     }
 }
