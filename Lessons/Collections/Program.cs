b5da80ee99e95d08d22d98e19c40d95459dc8775
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-10-27 15:16:39 +0500

    feat(collections): add non-generic collections

--- Lessons/Collections/Program.cs
+++ Lessons/Collections/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections;
 
 namespace MyNamespace
 {
@@ -6,7 +6,46 @@ namespace MyNamespace
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Collections");
+            NonGenericCollections.Run();
         }
+    }
+}
+
+class NonGenericCollections
+{
+
+    public static void Run()
+    {
+
+        // INFO: Pros: Dynamic resizing
+        // INFO: Cons: Boxing/unboxing, type safety issues
+
+        Console.WriteLine("\nArrayList\n");
+
+        ArrayList list = new ArrayList();
+        list.Add(10);
+        list.Add("Hello");
+        list.Add(3.14);
+        list.Add(true);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+
+        }
+        int x = (int)list[0]!;
+        Console.WriteLine(x);
+
+        // INFO: Pros: Fast key lookup
+        // INFO: Cons: No type safety
+
+        Console.WriteLine("\nHashtable\n");
+
+        Hashtable table = new Hashtable();
+        table["id"] = 101;
+        table["name"] = "Shohrux";
+        table["age"] = 18;
+
+        Console.WriteLine(table["name"]);
     }
 }
