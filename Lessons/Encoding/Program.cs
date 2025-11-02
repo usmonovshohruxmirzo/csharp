552e8d25c57e061c9bda216cc7ae14ae237f5c63
Shokhrukhmirzo Usmonov <shokhrukhmirzo_usmonov@student.itpu.uz>
2025-11-02 18:14:24 +0500

    chore: add Encoding in .NET

---
+++ Lessons/Encoding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Program
+{
+    class Program
+    {
+        // INFO: Unicode Transformation Format (UTF) = a method of representing Unicode characters as bytes.
+        static void Main(string[] args)
+        {
+            string text = "Hello";
+
+            byte[] utf8Bytes = Encoding.UTF8.GetBytes(text);
+            Console.WriteLine("UTF-8 Bytes: " + BitConverter.ToString(utf8Bytes));
+        }
+    }
+}
