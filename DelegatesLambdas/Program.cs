99d45b17f109040ed81ea5788f6f852be6518239
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-19 17:27:40 +0500

    refactor(deleges_and_lambdas): add curry function

--- DelegatesLambdas/Program.cs
+++ DelegatesLambdas/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Currying();
+        }
+
+
+        static void Currying()
+        {
+            Func<int, Func<int, Func<int, int>>> sum = x => y => z => x + y + z;
+            Console.WriteLine("SUM: {0}", sum(1)(2)(3));
+
+            Func<int, Func<int, Func<int, int>>> sum3 = x =>
+            {
+                Func<int, Func<int, int>> second = y =>
+                {
+                    Func<int, int> third = z =>
+                    {
+                        return x + y + z;
+                    };
+                    return third;
+                };
+                return second;
+            };
+            Console.WriteLine("SUM: {0}", sum3(1)(2)(3));
         }
     }
 }
