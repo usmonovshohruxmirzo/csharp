c935c73c9852a327411a3390856cdc1bf72290d2
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2025-11-19 17:27:17 +0500

    refactor: update Advanced String Manipulation

--- Lessons/AdvancedStringManipulation/Program.cs
+++ Lessons/AdvancedStringManipulation/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Program
+﻿using System.Text.RegularExpressions;
+
+namespace Program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            RegexClass.Run();
         }
     }
+}
+
+public class RegexClass
+{
+    public static void Run()
+    {
+        // 1. Dot .  -> any character except newline
+        Console.WriteLine(Regex.IsMatch("abc", "a.c")); // True (b matches .)
+
+        Console.WriteLine(Regex.IsMatch("color", "colou?r")); // True (matches both color/colour)
+    }
+
 }
