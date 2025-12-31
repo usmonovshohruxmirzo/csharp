2c7471ee7c12004e621c8865d3f3ba37a3732f54
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2025-12-31 10:26:29 +0500

    refactor(lessons): update Memory Management

--- Lessons/MemoryManagement/Program.cs
+++ Lessons/MemoryManagement/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MemoryManagement
+{
+    // INFO: In C#, memory management is primarily handled by the Common Language Runtime (CLR).
+    // As a developer, you don’t need to manually allocate or release memory, since the runtime manages it for you.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+
+        static void Calculate()
+        {
+            int a = 10; // stored in stack
+            int b = 50; // stored in stack
+            int result = a + b; // stored in stack
+        }
+    }
+}
