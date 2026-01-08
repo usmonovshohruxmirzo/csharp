28425a760416ca4ac83734ad29829ded3de9b1e8
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-08 18:04:58 +0500

    refactor: integrate StreamWriter and StreamReader to main Program

--- Lessons/StreamsAPI/Program.cs
+++ Lessons/StreamsAPI/Program.cs
@@ -6,7 +6,8 @@ namespace StreamsAPI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            SW.Run();
+            // SR.Run();
         }
     }
 }
