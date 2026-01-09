f793e274bd7b41a59a721a798a22ee65f9115608
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-09 21:47:20 +0500

    feat(lessons): add Http concept with simple get method example

---
+++ Lessons/Http/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Http
+{
+    class Program
+    {
+        public static readonly HttpClient _httpClient = new HttpClient();
+
+        public static async Task GetExampleTodoData()
+        {
+            string url = "https://jsonplaceholder.typicode.com/todos/1";
+            Console.WriteLine($"Sending GET request to: {url}");
+
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+            Console.WriteLine($"Status Code: {response.StatusCode} {response.ReasonPhrase}");
+            Console.WriteLine($"Is Success Status Code (2xx): {response.IsSuccessStatusCode}");
+
+            string responseBody = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("Response Body:");
+            Console.WriteLine(responseBody);
+            Console.ReadKey();
+        }
+
+        static async Task Main(string[] args)
+        {
+            await GetExampleTodoData();
+        }
+    }
+}
