5b3e12fcdd6f67515d9588a3bc29bcc9a9a4473e
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-30 15:09:20 +0500

    feat: add examples for preprocessor directives

--- Lessons/PreprocessorDirectives/Program.cs
+++ Lessons/PreprocessorDirectives/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#define DEBUG
+#define FEATURE_X
+
+using System;
+
+#nullable disable
+
+namespace PreprocessorDirectives
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = null;
+
+
+            #region Startup
+#line 100
+            Console.WriteLine("Application started");
+            #endregion
+
+#line default
+            Console.WriteLine("Back to normal");
+
+#if DEBUG
+#warning DEBUG mode is enabled
+            Console.WriteLine("Running in DEBUG mode");
+#elif RELEASE
+            Console.WriteLine("Running in RELEASE mode");
+#else
+#error Invalid configuration
+#endif
+
+#if FEATURE_X
+            Console.WriteLine("Feature X is enabled");
+#else
+            Console.WriteLine("Feature X is disabled");
+#endif
+
+#undef FEATURE_X
+
+#if FEATURE_X
+            Console.WriteLine("This will NOT compile");
+#else
+            Console.WriteLine("Feature X was undefined");
+#endif
+
+#pragma warning disable 0168
+            int unusedVariable;
+#pragma warning restore 0168
+
+            Console.WriteLine("Application finished");
+        }
+    }
+}
