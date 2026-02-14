98da2b04ec458653e83ce3d3df863bb68e9af84e
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-14 17:45:08 +0500

    refactor(program): integrated logger, dll import and more examples

--- Lessons/Attributes/Program.cs
+++ Lessons/Attributes/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Reflection;
+﻿#pragma warning disable
+
+using System.Reflection;
+
 namespace Attributes
 {
   class Program
@@ -9,6 +12,54 @@ namespace Attributes
       Console.WriteLine(assembly);
 
       MethodExample.Run();
+
+      Logger.Debug("Loading user...");
+      Logger.Debug("Connecting to DB...");
+
+      AccessingCustomAttributes();
+
+      DllImportExample.Run();
     }
+
+    static void AccessingCustomAttributes()
+    {
+      Type projectType = typeof(Project);
+      var classAttributes = projectType.GetCustomAttributes(typeof(DeveloperAttribute), false);
+      foreach (DeveloperAttribute attr in classAttributes)
+      {
+        Console.WriteLine($"Class Developer: {attr.Name}, Level: {attr.Level}");
+      }
+
+      MethodInfo[] methods = projectType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Static);
+      foreach (var method in methods)
+      {
+        var methodAttributes = method.GetCustomAttributes(typeof(DeveloperAttribute), false);
+        foreach (DeveloperAttribute attr in methodAttributes)
+        {
+          Console.WriteLine($"Method '{method.Name}' Developer: {attr.Name}, Level: {attr.Level}");
+        }
+      }
+    }
+  }
+
+  class Calculator
+  {
+    [Obsolete("Use AddNumbers instead of Add")]
+    public int Add(int a, int b)
+    {
+      return a + b;
+    }
+
+    public int AddNumbers(int a, int b)
+    {
+      return a + b;
+    }
+  }
+
+  [Serializable]
+  class Person
+  {
+    public string? Name { get; set; }
+    public int Age { get; set; }
   }
 }
