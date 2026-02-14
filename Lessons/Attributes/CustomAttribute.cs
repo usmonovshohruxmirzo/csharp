d1c189be1b4800f16c63a1d3bf5f322129b5c8b6
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-14 17:43:01 +0500

    feat(lesson, attributes): add custom attribute example

--- Lessons/Attributes/CustomAttribute.cs
+++ Lessons/Attributes/CustomAttribute.cs
@@ -1,7 +1,16 @@
-public class CustomAttribute : Attribute
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+class DeveloperAttribute(string name, string level) : Attribute
 {
-  public CustomAttribute()
-  {
+  public string Name { get; set; } = name;
+  public string Level { get; set; } = level;
+}
 
-  }
+[Developer("John", "Expert")]
+class Project
+{
+  [Developer("Alex", "Intermediate")]
+  public static void Start() { }
+
+  [Developer("Maria", "Beginner")]
+  public static void Test() { }
 }
