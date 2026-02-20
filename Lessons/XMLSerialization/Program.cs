881a6bc170040deda8ea6cf1a260a74a36b9f802
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-20 19:28:43 +0500

    feat(lessons): add xml serialization

---
+++ Lessons/XMLSerialization/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace XMLSerialization
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+
+}
+
+[XmlRoot("bookstrore")]
+class Bookstrore
+{
+  public List<Book>? Books { get; set; }
+}
+
+class Book
+{
+  [XmlAttribute("category")]
+  public string Category { get; set; }
+
+  [XmlAttribute("cover")]
+  public string Cover { get; set; }
+
+  [XmlElement("title")]
+  public Title Title { get; set; }
+
+  [XmlElement("author")]
+  public List<string> Authors { get; set; }
+
+  [XmlElement("year")]
+  public int Year { get; set; }
+
+  [XmlElement("price")]
+  public decimal Price { get; set; }
+}
+
+public class Title
+{
+  [XmlAttribute("lang")]
+  public string Lang { get; set; }
+
+  [XmlText]
+  public string Text { get; set; }
+}
