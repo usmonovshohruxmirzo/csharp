f0f8acac56f6eccb045db28c054742442f8f348d
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-20 19:29:09 +0500

    feat(lessons): add multi threading

---
+++ Lessons/MultiThreading/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace MultiThreading
+{
+  class Program
+  {
+    static readonly object locker = new();
+    static int counter = 0;
+    static void Main(string[] args)
+    {
+      Increment();
+      Console.WriteLine(counter);
+    }
+
+    static void Increment()
+    {
+      for (int i = 0; i < 100000; i++)
+      {
+        lock (locker)
+        {
+          counter++;
+        }
+      }
+    }
+  }
+}
