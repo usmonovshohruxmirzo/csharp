105ca3715d7d0eea7e6ff2e0c76070fc58583d22
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-21 10:00:04 +0500

    refactor(lessons): update xml serialization added more concepts

--- Lessons/XMLSerialization/Program.cs
+++ Lessons/XMLSerialization/Program.cs
@@ -53,7 +53,7 @@ class Program
         Console.WriteLine($"Category: {book.Category}, Cover: {book.Cover ?? "N/A"}");
         Console.WriteLine($"Title: {book?.Title?.Text} ({book?.Title?.Lang})");
         Console.WriteLine($"Year: {book?.Year}, Price: {book?.Price}");
-        
+
         foreach (var author in book?.Authors!)
         {
           Console.WriteLine($"Author: {author}");
@@ -126,5 +126,43 @@ class Program
 
       Console.WriteLine(new string('-', 20));
     }
+
+    var serializer2 = new XmlSerializer(typeof(Root));
+    using var reader = new StreamReader("with_prefix.xml");
+    Root result = (Root)serializer2.Deserialize(reader)!;
+    Console.WriteLine(result?.BookTable?.Title);
+    Console.WriteLine(result?.BookTable?.Author);
+    Console.WriteLine(result?.HtmlTable?.Row?.Cell);
   }
+}
+
+[XmlRoot("root")]
+public class Root
+{
+  [XmlElement("table", Namespace = "http://www.w3.org/TR/html4/")]
+  public HtmlTable? HtmlTable { get; set; }
+
+  [XmlElement("table", Namespace = "http://www.books.com/schema")]
+  public BookTable? BookTable { get; set; }
+}
+
+public class HtmlTable
+{
+  [XmlElement("tr", Namespace = "http://www.w3.org/TR/html4/")]
+  public HtmlRow? Row { get; set; }
+}
+
+public class HtmlRow
+{
+  [XmlElement("td", Namespace = "http://www.w3.org/TR/html4/")]
+  public string? Cell { get; set; }
+}
+
+public class BookTable
+{
+  [XmlElement("title", Namespace = "http://www.books.com/schema")]
+  public string? Title { get; set; }
+
+  [XmlElement("author", Namespace = "http://www.books.com/schema")]
+  public string? Author { get; set; }
 }
