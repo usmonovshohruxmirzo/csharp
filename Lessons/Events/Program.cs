1858afa570da1ad578d9884abb4ff77233d66c78
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-02-24 22:39:24 +0500

    refactor(Program): clean up commented code and improve delegate declaration

--- Lessons/Events/Program.cs
+++ Lessons/Events/Program.cs
@@ -1,7 +1,11 @@
 ﻿namespace Events
 {
+
   class Program
   {
+
+    public delegate void Test(int hello);
+
     static void Main(string[] args)
     {
       // Order order = new Order();
@@ -12,17 +16,20 @@
       // order.SetStatus("Paid");
       // order.SetStatus("Shipped");
 
-      CountdownTimer.Run();
+      // CountdownTimer.Run();
+
+      StockPriceAlertSystem.Run();
     }
 
-    static void SendEmail(string status)
-    {
-      Console.WriteLine($"Email sent: Order is {status}");
-    }
-
-    static void LogStatus(string status)
-    {
-      Console.WriteLine($"Log: Order status changed to {status}");
-    }
+    // static void SendEmail(string status)
+    // {
+    //   Console.WriteLine($"Email sent: Order is {status}");
+    // }
+    //
+    // static void LogStatus(string status)
+    // {
+    //   Console.WriteLine($"Log: Order status changed to {status}");
+    // }
   }
 }
+
