de226836518b06cefdbea172a778e75ff821e006
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-08-03 15:29:31 +0500

    feat: add network information

---
+++ Lessons/NetworkInformation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.NetworkInformation;
+
+namespace NetworkInformation;
+
+static class Program
+{
+  static async Task Main(string[] args)
+  {
+    bool isConnected = NetworkInterface.GetIsNetworkAvailable();
+    Console.WriteLine(isConnected ? "Connected" : "Disconnected");
+
+    var adapters = NetworkInterface.GetAllNetworkInterfaces();
+    foreach (var adapter in adapters)
+    {
+      PhysicalAddress mac = adapter.GetPhysicalAddress();
+      Console.WriteLine("{0} | {1} | {2}", adapter.Name, adapter.OperationalStatus, mac);
+    }
+
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine("PING EXAMPLE");
+    using Ping ping = new();
+    PingReply reply = await ping.SendPingAsync("google.com");
+    Console.WriteLine($"{reply.Address}\n{reply.Address}\n{reply.RoundtripTime}ms");
+    Console.ResetColor();
+
+    while (true)
+    {
+      using Ping ping1 = new();
+      PingReply reply1 = await ping.SendPingAsync("google.com");
+      Console.WriteLine($"{reply.Address} | {reply1.RoundtripTime}ms");
+      Thread.Sleep(200);
+    }
+  }
+}
