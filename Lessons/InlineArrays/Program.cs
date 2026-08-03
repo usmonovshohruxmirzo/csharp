211d5a1a80c2020d105e2c9c35b2f5b15f97b3bd
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-08-03 15:02:44 +0500

    feat: add inline arrays

---
+++ Lessons/InlineArrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace InlineArrays
+{
+  static class Program
+  {
+    static void Main()
+    {
+      MyBuffer buffer = default;
+
+      buffer[0] = 5;
+      buffer[1] = 10;
+
+      Console.WriteLine(buffer[1]);
+    }
+  }
+  [InlineArray(11)]
+  public struct MyBuffer
+  {
+    private int _element;
+  }
+}
