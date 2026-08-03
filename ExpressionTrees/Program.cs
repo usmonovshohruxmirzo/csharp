95a74c1ec80f703dbc1bff55214599777721b107
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-08-03 15:00:12 +0500

    feat: add expression trees

---
+++ ExpressionTrees/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+Expression<Func<int, int, int>> expr = (a, b) => a + b;
+
+Console.WriteLine(expr);
+Console.WriteLine(expr.Parameters[0]);
+Console.WriteLine(expr.Parameters[1]);
+
+var complied = expr.Compile();
+Console.WriteLine(complied(10,20));
