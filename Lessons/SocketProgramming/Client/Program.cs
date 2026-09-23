6e958e2d0143565085dc1b1cd0f3eeb99a39502a
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-23 23:00:40 +0500

    feat: add socket programming

---
+++ Lessons/SocketProgramming/Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+      IPAddress ipAddr = ipHost.AddressList[0];
+      IPEndPoint localEndpoint = new(ipAddr, 11111);
+      Socket sender = new(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+
+      try
+      {
+        sender.Connect(localEndpoint);
+
+        Console.WriteLine("Socket connected to -> {0}", sender.RemoteEndPoint.ToString());
+
+        byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+        int byteSent = sender.Send(messageSent);
+
+        byte[] messageReceived = new byte[1024];
+
+        int byteRecv = sender.Receive(messageReceived);
+        Console.WriteLine("Message from Server -> {0}", Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+        sender.Shutdown(SocketShutdown.Both);
+        sender.Close();
+      }
+
+      catch (ArgumentNullException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+
+      catch (SocketException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
---
+++ Lessons/SocketProgramming/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+  static class Program
+  {
+    static void Main(string[] args)
+    {
+      IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+      IPAddress ipAddr = ipHost.AddressList[0];
+      IPEndPoint localEndpoint = new IPEndPoint(ipAddr, 11111);
+
+      Socket listener = new(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+      try
+      {
+        listener.Bind(localEndpoint);
+
+        listener.Listen(10);
+
+        while (true)
+        {
+          Console.WriteLine("Waiting for connection...");
+
+          Socket clientSocket = listener.Accept();
+
+          byte[] bytes = new byte[1024];
+          string data = null!;
+
+          while (true)
+          {
+            int numByte = clientSocket.Receive(bytes);
+            data += Encoding.ASCII.GetString(bytes, 0, numByte);
+
+            if (data.IndexOf("<EOF>") > -1)
+            {
+              break;
+            }
+          }
+
+          Console.WriteLine("Text received -> {0} ", data);
+
+          byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+          clientSocket.Send(message);
+
+          clientSocket.Shutdown(SocketShutdown.Both);
+          clientSocket.Close();
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
